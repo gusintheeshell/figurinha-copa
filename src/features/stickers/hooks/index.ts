a0befde6d91b5939268a51bdc85d463f825589cd
Gustavo Barbosa <gusbdev@gmail.com>
2026-05-26 19:26:53 -0300

    feat: add exports for useSwipeGesture and useStickerInteractions hooks to enhance sticker functionality

--- src/features/stickers/hooks/index.ts
+++ src/features/stickers/hooks/index.ts
@@ -1,2 +1,4 @@
 export { useLongPress } from './useLongPress'
+export { useSwipeGesture } from './useSwipeGesture'
+export { useStickerInteractions } from './useStickerInteractions'
 export { filterStickers, useStickerStats } from './useStickerStats'
